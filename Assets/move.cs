e8a99cbc20810b0fcde83317e2ce6121bda9177c
nastyakit <anastasijakitaeva@gmail.com>
2017-09-26 00:28:53 +0300

    Platform move in two side

--- Assets/move.cs
+++ Assets/move.cs
@@ -2,23 +2,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+//using System.Diagnostics;
 using UnityEngine;
 
-public class move : MonoBehaviour {
+public class move : MonoBehaviour
+{
     public Transform EndPoint;
-    //Rigidbody rk;
-    //System.Random rand = new System.Random();
+    //System.Diagnostics.Stopwatch sw = new Stopwatch();
+    //int k = 0;
+    bool m = true;
+    Vector3 StartPoint;
 
     void Start ()
     {
-        //GetComponent<Renderer>().material.color = Color.green;
-        //rk = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
+        StartPoint = transform.position;
+    }
+
 	void Update () {
-        //rk.velocity = new Vector3(0, 5, 0);
-        //transform.Translate(new Vector3(rand.Next(60) / 10 - 3, rand.Next(30) / 10, rand.Next(60) / 10 - 3) * Time.deltaTime);
-        transform.position = Vector3.MoveTowards(transform.position, EndPoint.position, Time.deltaTime);
+        //k += 1;
+        //sw.Stop();
+        //if ((sw.ElapsedMilliseconds / 100.0) % 2 == 0) { 
+        //if (k % 2 == 1) {
+        if (m==true && transform.position==EndPoint.position)
+        {
+            m = false;
+        }
+        else if (m==false && transform.position==StartPoint)
+        {
+            m = true;
+        }
+        if (m) {
+            transform.position = Vector3.MoveTowards(transform.position, EndPoint.position, Time.deltaTime);
+        }
+        else {
+            transform.position = Vector3.MoveTowards(transform.position, StartPoint, Time.deltaTime);
+        }
     }
 }
